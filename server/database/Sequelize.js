471bdd87370760f635cf3fe651a76dda2db3b581
alvarocarrerasr <alvarocarrerasr@gmail.com>
2017-10-19 19:00:31 +0200

    Sessions and Sequelize

--- server/database/Sequelize.js
+++ server/database/Sequelize.js
@@ -1,13 +1,33 @@
 const settings = require('./../settings.json');
 const Sequelize = require('sequelize') ;
+
+
 const DATABASE_SETTINGS = settings.database;
 var database = new Sequelize(
-    DATABASE_SETTINGS.database,
-    DATABASE_SETTINGS.username, 
-    DATABASE_SETTINGS.password);
+    DATABASE_SETTINGS.dbName,
+    DATABASE_SETTINGS.dbUsername, 
+    DATABASE_SETTINGS.dbPassword,
+    {
+        host:DATABASE_SETTINGS.dbHost,
+        dialect:DATABASE_SETTINGS.dbType,
+        pool:{
+            max:5,
+            min:0,
+            idle:10000
+        }
+    }
+);
+
+database.authenticate()
+.then(()=>{
+    console.log("Connected to database successfully");
+})
+.catch((err)=>{
+    console.log("Error while connecting to DB", err);
+})
 
 
 
-modle.exports ={
+module.exports ={
     sequelize:database
 };
