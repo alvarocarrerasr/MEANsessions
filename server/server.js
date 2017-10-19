dfad6581d62dcca7860fb8cde1582fdc18dfb1e2
alvarocarrerasr <alvarocarrerasr@gmail.com>
2017-10-19 19:48:41 +0200

    Sessions

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 3789;
 
 
 app.post('/login',(req, resp)=>{
+    var username = req.body.usernameLogin;
+    var password = req.body.passwordLogin;
+    req.session.username=username;
+    console.log(req);
+    resp.destroy();
+    console.log("Login from", username, password);
+    console.log(req.session);
 
 });
 
